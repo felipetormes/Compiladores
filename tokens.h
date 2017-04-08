f89f04a5367b12c95a9ea8b1074b892a1794e035
Felipe Tormes <felipe.tormes@gmail.com>
2017-04-08 01:54:41 +0100

    MODIFY: change tokens

--- tokens.h
+++ tokens.h
@@ -7,13 +7,15 @@
  * valor ascii de caracteres isolados.
  */
 
-#define KW_INT        256
-#define KW_FLOAT      257
-#define KW_BOOL       258
-#define KW_CHAR       259
-#define KW_IF         261
+#define KW_BYTE       256
+#define KW_SHORT      257
+#define KW_LONG       258
+#define KW_FLOAT      259
+#define KW_DOUBLE     260
+#define KW_WHEN       261
 #define KW_THEN       262
 #define KW_ELSE       263
+#define KW_WHILE      264
 #define KW_FOR        265
 #define KW_READ       266
 #define KW_RETURN     267
@@ -28,8 +30,7 @@
 
 #define TK_IDENTIFIER 280
 #define LIT_INTEGER   281
-#define LIT_FALSE     283
-#define LIT_TRUE      284
+#define LIT_REAL      282
 #define LIT_CHAR      285
 #define LIT_STRING    286
 
