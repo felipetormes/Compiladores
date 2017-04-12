d21210c419bd32e7a9f4d6bd60384964fc518c82
Felipe Tormes <fbtormes@inf.ufrgs.br>
2017-04-12 08:19:53 -0300

    MODIFY: test if the file can be opened

--- main.c
+++ main.c
@@ -5,7 +5,19 @@ int main(int argc, char** argv){
 
   initMe();
 
+  if(argc < 2)
+  {
+    printf("\nPlease, write the file name after the executable's name!\n\n");
+    exit(0);
+  }
+
   FILE* file = fopen(argv[1], "r");
+
+  if(!file)
+  {
+    printf("\nThe file %s cant't be opened or you typed the wrong name!\n\n", argv[1]);
+    exit(1);
+  }
 
   yyin = file;
 
