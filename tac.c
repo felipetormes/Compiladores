12acd7df7866462bde70ddccec984b1e00a84d76
Felipe Tormes <fbtormes@inf.ufrgs.br>
2017-06-20 15:55:26 -0300

    UPDATE: create TACs of symbols, arithmatic operations and flow control

--- tac.c
+++ tac.c
@@ -6,12 +6,12 @@
 #include "hash.h"
 #include "tac.h"
 
-TAC* tacCreate(enum tac_type_enum type, hashNode* destination, hashNode* source1, hashNode* source2)
+TAC* tacCreate(enum tac_type_enum type, hashNode* res, hashNode* source1, hashNode* source2)
 {
 	TAC* newTac = malloc(sizeof(TAC));
 
 	newTac->tac_type = type;
-	newTac->destination = destination;
+	newTac->res = res;
 	newTac->source1 = source1;
 	newTac->source2 = source2;
 	newTac->prev = NULL;
@@ -39,4 +39,261 @@ TAC* tacJoin(TAC* tac1, TAC* tac2)
 	tac1->next = aux;
 
 	return tac2;
+}
+
+TAC* clone(TAC* original)
+{
+	TAC* tac_clone = tacCreate(original->tac_type, original->res, original->source1, original->source2);
+
+	TAC* clone_ptr = tac_clone->prev;
+	TAC* original_ptr = original->prev;
+
+	while(original_ptr != NULL)
+	{
+		clone_ptr = tacCreate(original_ptr->tac_type, original_ptr->res, original_ptr->source1, original_ptr->source2);
+		clone_ptr->prev = original_ptr->prev;
+
+		original_ptr = original_ptr->prev;
+		clone_ptr = clone_ptr->prev;
+	}
+
+	return tac_clone;
+}
+
+hashNode* makeTemp()
+{
+	static int count = 0;
+	char* tempName = malloc(10 + sizeof(int) * 8 + 1);
+
+	sprintf(tempName, "___temp%d___", count);
+	count++;
+
+	return hashInsert(tempName, SYMBOL_IDENTIFIER);
+}
+
+hashNode* makeLabel()
+{
+	static int count = 0;
+	char* labelName = malloc(11 + sizeof(int) * 8 + 1);
+
+	sprintf(labelName, "___label%d___", count);
+	count++;
+
+	return hashInsert(labelName, SYMBOL_IDENTIFIER);
+}
+
+TAC* reverse(TAC* myTac)
+{
+	if(myTac == NULL)
+		return NULL;
+
+	TAC* aux = myTac;
+
+	while(aux->prev != NULL)
+	{
+		aux = aux->prev;
+	}
+
+	return aux;
+}
+
+TAC* tacArithmeticOp(enum tac_type_enum type, TAC** children)
+{
+	hashNode* temp1 = children[0]->res;
+	hashNode* temp2 = children[1]->res;
+
+	return tacJoin(tacJoin(children[0], children[1]), tacCreate(type, makeTemp(), temp1, temp2));
+}
+
+TAC* tacIfZero(TAC* test, TAC* thenBlock, TAC* elseBlock)
+{
+	TAC* result;
+	hashNode* testResult = test->res;
+	hashNode* elseLabel = makeLabel();
+
+	TAC* ifThen = tacJoin(tacJoin(test, tacCreate(TAC_IFZ, elseLabel, testResult, NULL)), thenBlock);
+
+	if(elseBlock == NULL)
+	{
+		result = tacJoin(ifThen, tacCreate(TAC_LABEL, elseLabel, NULL, NULL));
+	}
+	else
+	{
+		hashNode* endLabel = makeLabel();
+
+		result =
+			tacJoin(
+				tacJoin(
+					tacJoin(
+						tacJoin(
+							ifThen,
+							tacCreate(TAC_JUMP, endLabel, NULL, NULL)
+						),
+						tacCreate(TAC_LABEL, elseLabel, NULL, NULL)
+					),
+					elseBlock
+				),
+				tacCreate(TAC_LABEL, endLabel, NULL, NULL)
+			);
+	}
+
+	return result;
+}
+
+TAC* tacWhile(TAC* test, TAC* whileBlock)
+{
+	hashNode* testResult = test->res;
+	hashNode* loopLabel = makeLabel();
+	hashNode* endLabel = makeLabel();
+
+	TAC* new_test = clone(test);
+
+	return
+		tacJoin(
+			tacJoin(
+				tacJoin(
+					tacJoin(
+						test,
+						tacJoin(
+							tacCreate(TAC_LABEL, loopLabel, NULL, NULL),
+							tacCreate(TAC_IFZ, endLabel, testResult, NULL)
+						)
+					),
+					whileBlock
+				),
+				tacJoin(
+					new_test,
+					tacCreate(TAC_JUMP, loopLabel, NULL, NULL)
+				)
+			),
+			tacCreate(TAC_LABEL, endLabel, NULL, NULL)
+		);
+}
+
+TAC* tacFor(TAC* atr, TAC* lit, TAC* forBlock)
+{
+
+}
+
+TAC* tacGenerate(astree* ast)
+{
+	if(ast == NULL)
+		return NULL;
+
+	TAC* childTac[4];
+
+	childTac[0] = tacGenerate(ast->child[0]);
+	childTac[1] = tacGenerate(ast->child[1]);
+	childTac[2] = tacGenerate(ast->child[2]);
+	childTac[3] = tacGenerate(ast->child[3]);
+
+	TAC* result;
+
+	switch (ast->node_type)
+	{
+		case LITERAL:
+		{
+			result = tacCreate(TAC_SYMBOL, ast->node, NULL, NULL);
+			break;
+		}
+
+		case IDENTIFIER:
+		{
+			result = tacCreate(TAC_SYMBOL, ast->node, NULL, NULL);
+			break;
+		}
+
+		case ADDITION:
+		{
+			result = tacArithmeticOp(TAC_ADD, childTac);
+			break;
+		}
+
+		case SUBTRACTION:
+		{
+			result = tacArithmeticOp(TAC_SUB, childTac);
+			break;
+		}
+
+		case MULTIPLICATION:
+		{
+			result = tacArithmeticOp(TAC_MUL, childTac);
+			break;
+		}
+
+		case DIVISION:
+		{
+			result = tacArithmeticOp(TAC_DIV, childTac);
+			break;
+		}
+
+		case LESSERTHAN:
+		{
+			result = tacArithmeticOp(TAC_LESS, childTac);
+			break;
+		}
+
+		case GREATERTHAN:
+		{
+			result = tacArithmeticOp(TAC_GREATER, childTac);
+			break;
+		}
+
+		case LESSEREQUAL:
+		{
+			result = tacArithmeticOp(TAC_LESS_EQUAL, childTac);
+			break;
+		}
+
+		case GREATEREQUAL:
+		{
+			result = tacArithmeticOp(TAC_GREATER_EQUAL, childTac);
+			break;
+		}
+
+		case EQUAL:
+		{
+			result = tacArithmeticOp(TAC_EQUAL, childTac);
+			break;
+		}
+
+		case NOTEQUAL:
+		{
+			result = tacArithmeticOp(TAC_NOT_EQUAL, childTac);
+			break;
+		}
+
+		case AND:
+		{
+			result = tacArithmeticOp(TAC_AND, childTac);
+			break;
+		}
+
+		case OR:
+		{
+			result = tacArithmeticOp(TAC_OR, childTac);
+			break;
+		}
+
+		case WHENTHEN:
+		case WHENTHENELSE:
+		{
+			result = tacIfZero(childTac[0], childTac[1], childTac[2]);
+			break;
+		}
+
+		case WHILE:
+		{
+			result = tacWhile(childTac[0], childTac[1]);
+			break;
+		}
+
+		case FOR:
+		{
+			result = tacFor(childTac[0], childTac[1], childTac[2]);
+			break;
+		}
+
+	}
+
 }
--- tac.h
+++ tac.h
@@ -1,5 +1,8 @@
 //Cássio de Abreu Ramos, Felipe Barbosa Tormes
 
+#ifndef TAC_H
+#define TAC_H
+
 #include "hash.h"
 
 enum tac_type_enum
@@ -7,7 +10,17 @@ enum tac_type_enum
   TAC_SYMBOL,
   TAC_MOVE,
   TAC_ADD,
+  TAC_SUB,
   TAC_MUL,
+  TAC_DIV,
+  TAC_LESS,
+  TAC_GREATER,
+  TAC_LESS_EQUAL,
+  TAC_GREATER_EQUAL,
+  TAC_EQUAL,
+  TAC_NOT_EQUAL,
+  TAC_AND,
+  TAC_OR,
   TAC_LABEL,
   TAC_BEGINFUN,
   TAC_ENDFUN,
@@ -18,7 +31,7 @@ enum tac_type_enum
   TAC_RET,
   TAC_PRINT,
   TAC_READ
-}
+};
 
 typedef struct tac_struct
 {
@@ -34,3 +47,14 @@ typedef struct tac_struct
 
 TAC* tacCreate(enum tac_type_enum type, hashNode* destination, hashNode* source1, hashNode* source2);
 TAC* tacJoin(TAC* tac1, TAC* tac2);
+TAC* clone(TAC* original);
+hashNode* makeTemp();
+hashNode* makeLabel();
+TAC* reverse(TAC* myTac);
+TAC* tacArithmeticOp(enum tac_type_enum type, TAC** children);
+TAC* tacIfZero(TAC* test, TAC* thenBlock, TAC* elseBlock);
+TAC* tacWhile(TAC* test, TAC* whileBlock);
+TAC* tacFor(TAC* atr, TAC* lit, TAC* forBlock);
+TAC* tacGenerate(astree* ast);
+
+#endif
